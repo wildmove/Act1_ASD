71b275ff284aeebf2249fe76cce164c62e99d97f
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-10-07 17:16:13 +0700

    Deklarasi array int, variabel int, dan menambahkan method untuk menerima masukkan

--- Act1_ASD/Program.cs
+++ Act1_ASD/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada aaray
+        private int n;
+
+        //Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+                Console.WriteLine("");
+                Console.WriteLine("-----------------------");
+                Console.WriteLine(" Masukkan elemen array ");
+                Console.WriteLine("-----------------------");
+
+                // Pengguna memasukkan elemen pada array
+                for (int i = 0; i < n; i++) {
+                    Console.Write("<" + (i + 1) + "> ");
+                    string s1 = Console.ReadLine();
+                    a[1] = Int32.Parse(s1);
+                }
+
+            }
+        }
+    }
+}
